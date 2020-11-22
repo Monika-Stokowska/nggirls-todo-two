68bfc1b10ce9d5855bef4e1f6ccadd34f488d4e6
Monika Stokowska <monika_stokowska@tlen.pl>
2020-11-22 08:59:14 +0100

    first commit

--- todo-list-two/src/app/app.component.ts
+++ todo-list-two/src/app/app.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  styleUrls: ['./app.component.scss'],
   template: `
     <h1>Welcome to {{ title }}!</h1>
 
     <app-input-button-unit></app-input-button-unit>
   `,
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'todo-list-two';
 }
--- todo-list-two/src/app/input-button-unit/input-button-unit.component.ts
+++ todo-list-two/src/app/input-button-unit/input-button-unit.component.ts
@@ -7,4 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputButtonUnitComponent implements OnInit {
   title = 'Hello World';
+  constructor() {}
+
+  ngOnInit(): void {}
 }
